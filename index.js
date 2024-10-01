506f57e86b250f9b6766ffa3c7bf6a4e0dff3efb
smartinez254 <smartinez254@student.cccd.edu>
2024-10-01 01:04:40 -0700

    Commit

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ function getLocalDate(month, day, year) {
   // TODO: Implement this function.
   const result = 'undefined';
   return result;
+  
+  getLocalDate(7, 24, 2005);
 }
 
 // DO NOT CHANGE ANYTHING BELOW THIS LINE.
